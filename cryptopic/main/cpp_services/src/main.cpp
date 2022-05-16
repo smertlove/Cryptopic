a6f0b43f7c13b4fe225c25b3fb6472321b973f81
Kirill Soloshenko <smertlove@yandex.ru>
2022-05-16 03:56:00 +0300

    linked Cpp and Python

--- cryptopic/main/cpp_services/src/main.cpp
+++ cryptopic/main/cpp_services/src/main.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <cstdlib>
 
-// class ClientDataManager {
-//     public:
-//         // void receive_data(std::string str) {
-//         //     return;
-//         // }
-
-//         // std::string send_data(std::string str) {
-//         //     return str;
-//         // }
-
-//         int manage_data(int str) {
-//              std::cout << "2 " << str << std::endl;
-//             return str;
-//         }
-// };
 
 extern "C" {
-    // ClientDataManager* make_manager(){ return new ClientDataManager(); }
-    // void call_receive_data(ClientDataController* ctrl, std::string str) { ctrl->receive_data(str); }
     char* call_manage_data(char* str)    {
-        for (size_t i; i < 10; i++){
-            std::cout << *str << std::endl;
-            str++;
-        }
-         return str; }
+        std::cout << "c++ func called" << std::endl;
+        char* ptr = str;
+        *(ptr + 5) = 'R';
+        *(ptr + 6) = 'U';
+        return str;
+    }
 }
--- cryptopic/main/views.py
+++ cryptopic/main/views.py
@@ -8,27 +8,16 @@ from ctypes import cdll
 
 
 lib = cdll.LoadLibrary('../cryptopic/main/cpp_services/bin/libmain.so')
-# lib.call_manage_data.argtypes = (ClientDataManager, ctypes.c_int)
+lib.call_manage_data.restype = ctypes.c_char_p
+lib.call_manage_data.argtypes = [ctypes.c_char_p]
 
-# class ClientDataManager:
-#     def __init__(self):
-#         self.obj = lib.make_manager()
 
-#     def manage(self, str):
-#         lib.call_manage_data(self.obj, str)
-
-# import ctypes
-
-# print("!!!!!!!!!_____________!!!!!!!!!!!!!_______________!!!!!!!!!!!!!")
-# test = ctypes.CDLL('./objs/libtest.so')
-
-# gcc -fPIC -shared -o ./cpp_services/bin/libmain.so ./cpp_services/src/main.cpp
+# g++ -fPIC -shared -o ./cpp_services/bin/libmain.so ./cpp_services/src/main.cpp
 def index(request):
-    # userform = UserForm()
     return render(request, 'main/index.html')
 
 def req(request):
-    # a = ClientDataManager()
-    b = lib.call_manage_data(ctypes.create_unicode_buffer("privet zdarova", len("privet zdarova")))
+    b = lib.call_manage_data(b"Privet, C++!")
+    b += b" works nicely"
     print(b)
     return JsonResponse({"msg": "Greetings, fellow user"}, content_type="application/json")
