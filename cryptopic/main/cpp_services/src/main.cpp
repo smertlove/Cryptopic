29404221bae8e91d031bc1849cb98b46874d61c3
Kirill Soloshenko <smertlove@yandex.ru>
2022-05-16 19:38:39 +0300

    ...

--- cryptopic/main/cpp_services/src/main.cpp
+++ cryptopic/main/cpp_services/src/main.cpp
@@ -11,15 +11,19 @@
 
 using namespace macaron;
 
+
+
 extern "C" {
-    char* call_manage_data(char* str, char* txt)    {
+    char* call_manage_data(char* str, char* txt, char* name)    {
 
         while (*str != ',') {str++;}
         str++;
 
         std::cout << "c++ func called" << std::endl;
-        size_t size = strlen(str);
-        std::string img_b64(str, size);
+
+
+        std::string img_b64(str);
+
         std::string img_decoded;
         std::cout << "char converted to string" << std::endl;
 
@@ -27,14 +31,14 @@ extern "C" {
         std::cout << "init encoder" << std::endl;
         B64Encoder->Decode(img_b64, img_decoded);
         std::cout << "decode" << std::endl;
-        size_t size2 = img_decoded.length();
+        
 
         std::ofstream fout;
-        fout.open("../output_imgs/" + std::string(txt) + ".png", std::ios::app |std::ios::binary);
+        fout.open("../output_imgs/" + std::string(name) + ".png", std::ios::app |std::ios::binary);
         std::cout << "save" << std::endl;
         fout << img_decoded;
-        // fout.write(img_decoded, size2);
         fout.close();
+
         return str;
     }
 }
--- cryptopic/main/views.py
+++ cryptopic/main/views.py
@@ -3,6 +3,8 @@ from django.views.generic import DetailView
 from django.http import JsonResponse
 from django.http import QueryDict
 
+from random import randint
+
 import ctypes
 from ctypes import cdll
 
@@ -11,7 +13,7 @@ import json
 
 lib = cdll.LoadLibrary('../cryptopic/main/cpp_services/bin/libmain.so')
 lib.call_manage_data.restype = ctypes.c_char_p
-lib.call_manage_data.argtypes = [ctypes.c_char_p, ctypes.c_char_p]
+lib.call_manage_data.argtypes = [ctypes.c_char_p, ctypes.c_char_p , ctypes.c_char_p]
 
 
 # g++ -fPIC -shared -o ./cpp_services/bin/libmain.so ./cpp_services/src/main.cpp
@@ -19,6 +21,9 @@ def index(request):
     return render(request, 'main/index.html')
 
 def req(request):
+    name = ''.join(list(map(str, [randint(1, 7) for _ in range(10)])))
     front_body = json.loads(request.body.decode('utf-8'))
-    pic = lib.call_manage_data(bytes(front_body["picture"], encoding="utf-8"), bytes(front_body["text"], encoding="utf-8"))
+    pic = lib.call_manage_data(bytes(front_body["picture"], encoding="utf-8"),
+     bytes(front_body["text"], encoding="utf-8"),
+     bytes(name, encoding="utf-8"))
     return JsonResponse(front_body, content_type="application/json")
