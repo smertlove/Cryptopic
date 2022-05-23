6de08a615a78f7c035f903256f62368bdb58ef05
Kirill Soloshenko <smertlove@yandex.ru>
2022-05-23 22:24:10 +0300

    name generation for imgs moved to cpp file

--- cryptopic/main/cpp_services/src/main.cpp
+++ cryptopic/main/cpp_services/src/main.cpp
@@ -2,43 +2,64 @@
 #include <fstream>
 
 #include <string>
+
 #include <cstring>
 #include <cstdlib>
+#include <cstdio>
 
 #include <random>
 
+#include <opencv2/core/core.hpp>
+
 #include "Base64.h"
+#include "lsb_encryptor.hpp"
+
 
 using namespace macaron;
 
+std::string get_random_name(size_t start, size_t finish, size_t n) {
+    std::random_device random_device;
+    std::mt19937 generator(random_device());
+    std::uniform_int_distribution<char> dis(start, finish);
 
+    std::string result;
+    for (size_t i = 0; i < n; i++) {
+        result += dis(generator);
+    }
+    return result;
+}
+
+std::string decode_img(char* b64_img, Base64 *b64_manager) {
+    // get rid of useless b64 part
+    while (*b64_img != ',') {++b64_img;}
+    ++b64_img;
+
+    // call decoder
+    std::string img_decoded;
+    b64_manager->Decode(std::string(b64_img), img_decoded);
+
+    return img_decoded;
+}
 
 extern "C" {
-    char* call_manage_data(char* str, char* txt, char* name)    {
+    char* call_manage_data(char* b64_img, char* txt, char* name)    {
 
-        while (*str != ',') {str++;}
-        str++;
+        // init b64 manager
+        Base64 *B64Manager = new Base64();
 
-        std::cout << "c++ func called" << std::endl;
+        // call decoder
+        std::string img_decoded = decode_img(b64_img, B64Manager);
 
-
-        std::string img_b64(str);
-
-        std::string img_decoded;
-        std::cout << "char converted to string" << std::endl;
-
-        Base64 *B64Encoder = new Base64();
-        std::cout << "init encoder" << std::endl;
-        B64Encoder->Decode(img_b64, img_decoded);
-        std::cout << "decode" << std::endl;
-        
-
+        // save img with random name
         std::ofstream fout;
-        fout.open("../output_imgs/" + std::string(name) + ".png", std::ios::app |std::ios::binary);
+        std::string filename = "../output_imgs/" + get_random_name(65, 90, 20) + ".png";
+        fout.open(filename, std::ios::app |std::ios::binary);
         std::cout << "save" << std::endl;
         fout << img_decoded;
         fout.close();
 
-        return str;
+        // remove("../output_imgs/fff.txt");
+
+        return b64_img;
     }
 }
