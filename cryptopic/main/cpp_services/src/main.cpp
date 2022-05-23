a2b57d7a65315ec840e422a47bc4ee2853d9fcc4
Kirill Soloshenko <smertlove@yandex.ru>
2022-05-23 23:02:40 +0300

    skjdfhkjlsadhjasdkljhfdasljhdfsakljljfdsjklh!!!!!!!!!!!!!!!!!!!!

--- cryptopic/main/cpp_services/src/main.cpp
+++ cryptopic/main/cpp_services/src/main.cpp
@@ -10,6 +10,7 @@
 #include <random>
 
 #include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 #include "Base64.h"
 #include "lsb_encryptor.hpp"
@@ -52,13 +53,24 @@ extern "C" {
 
         // save img with random name
         std::ofstream fout;
-        std::string filename = "../output_imgs/" + get_random_name(65, 90, 20) + ".png";
-        fout.open(filename, std::ios::app |std::ios::binary);
+        std::string filename = get_random_name(65, 90, 20) + ".png";
+        fout.open("../output_imgs/" + filename, std::ios::app |std::ios::binary);
         std::cout << "save" << std::endl;
         fout << img_decoded;
         fout.close();
 
+        // magic happens!!
+        cv::Mat matrix_img = cv::imread("../output_imgs/" + filename);
+        matrix_img = encode_lsb(matrix_img, std::string(txt));
+        cv::imwrite("../output_encoded/" + filename, matrix_img);
+        std::cout << "finish encoding" << std::endl;
         // remove("../output_imgs/fff.txt");
+
+
+
+        std::cout << "decode try: " << std::endl;
+        
+        std::cout << decode_lsb(matrix_img) << std::endl;
 
         return b64_img;
     }
